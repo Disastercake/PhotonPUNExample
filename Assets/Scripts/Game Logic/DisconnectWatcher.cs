065a6440dbeb59889b70e563614dc04d8837b6f6
Disastercake <mike@disastercake.com>
2022-09-28 03:41:11 -0400

    Create: DisconnectWatcher.cs - Reloads a scene when disconnected.

---
+++ Assets/Scripts/Game Logic/DisconnectWatcher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Photon.Pun;
+using UnityEngine;
+
+namespace PhotonPunExample
+{
+    [DisallowMultipleComponent]
+    public class DisconnectWatcher : MonoBehaviour
+    {
+        [SerializeField, Tooltip("This component will load this scene when disconnected from Photon.")]
+        private string _sceneName;
+        
+        private void Update()
+        {
+            if (PhotonNetwork.IsConnected)
+                Debug.LogError("Disconnected from Photon server.");
+        }
+    }
+}
