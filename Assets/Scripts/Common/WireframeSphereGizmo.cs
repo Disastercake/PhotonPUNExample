f19a2a5631a8c966d0c48becad72a2ca999ed135
Disastercake <mike@disastercake.com>
2022-09-28 03:33:45 -0400

    Create: WireframeSphereGizmo.cs - Editor-only gizmo for easily viewing invisible objects in the scene view.

---
+++ Assets/Scripts/Common/WireframeSphereGizmo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Common
+{
+    [DisallowMultipleComponent]
+    public class WireframeSphereGizmo : MonoBehaviour
+    {
+#if UNITY_EDITOR
+
+        [SerializeField] private Color _color = Color.white;
+        [SerializeField] private bool _wireframe = true;
+        [SerializeField] private float _radius = 1f;
+        
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = _color;
+            
+            if (_wireframe)
+                Gizmos.DrawWireSphere(transform.position, _radius);
+            else
+                Gizmos.DrawSphere(transform.position, _radius);
+        }
+
+#endif
+    }
+}
