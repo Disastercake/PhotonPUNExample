607ef20dad6eef143ca98a0238acf682ba0cf2be
Disastercake <mike@disastercake.com>
2022-09-28 08:51:05 -0400

    SceneHandler.cs - Removed all logic.  The IPUNInstantiateMagicCallback apparently only works directly on prefabs that were instantiated and is not a global message.

--- Assets/Scripts/Game Logic/SceneHandler.cs
+++ Assets/Scripts/Game Logic/SceneHandler.cs
@@ -1,32 +1,10 @@
-﻿using System;
-using Photon.Pun;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace PhotonPunExample
 {
     [DisallowMultipleComponent]
-    public class SceneHandler : MonoBehaviour, IPunInstantiateMagicCallback
+    public class SceneHandler : MonoBehaviour
     {
-        private void OnEnable()
-        {
-            PhotonNetwork.AddCallbackTarget(this);
-        }
-
-        private void OnDisable()
-        {
-            PhotonNetwork.AddCallbackTarget(this);
-        }
-
-        void IPunInstantiateMagicCallback.OnPhotonInstantiate(PhotonMessageInfo info)
-        {
-            Debug.Log("OnPhotonInstantiate()");
-            
-            object[] instantiationData = info.photonView.InstantiationData;
-
-            if (instantiationData.Length <= 0)
-                return;
-
-            info.photonView.gameObject.name = instantiationData[0] as string ?? $"NO_NAME ({info.photonView.ViewID})";
-        }
+        
     }
 }
