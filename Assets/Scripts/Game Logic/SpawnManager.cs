3572f031b2c6d92de428b000f28fd5c0ee5d567d
Disastercake <mike@disastercake.com>
2022-09-28 03:21:18 -0400

    SpawnManager.cs - Added basic logic for spawning a player prefab.

--- Assets/Scripts/Game Logic/SpawnManager.cs
+++ Assets/Scripts/Game Logic/SpawnManager.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using Photon.Pun;
+using UnityEngine;
 
 namespace PhotonPunExample
 {
@@ -6,5 +8,36 @@ namespace PhotonPunExample
     public class SpawnManager : MonoBehaviour
     {
         
+        #region Singleton
+
+        public static SpawnManager Instance { get; private set; } = null;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        #endregion
+
+        [SerializeField] private GameObject _pfPlayer;
+        [SerializeField] private Transform[] _spawnPoints;
+
+        public Transform GetRandomSpawnPoint()
+        {
+            return _spawnPoints[UnityEngine.Random.Range(0, _spawnPoints.Length)];
+        }
+        
+        private void Start()
+        {
+            if (PhotonNetwork.IsConnected)
+                SpawnPlayer();
+        }
+
+        private void SpawnPlayer()
+        {
+            Transform spawnPoint = GetRandomSpawnPoint();
+
+            PhotonNetwork.Instantiate(_pfPlayer.name, spawnPoint.position, spawnPoint.rotation);
+        }
     }
 }
