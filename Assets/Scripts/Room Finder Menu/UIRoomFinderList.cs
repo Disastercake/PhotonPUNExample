94f04f01ffb57f9dc3fda01008ccbe4abf8d2068
Disastercake <mike@disastercake.com>
2022-09-28 06:26:11 -0400

    UIRoomFinderList.cs - Added Initialize() pattern to make sure everything is initialized when needed.

--- Assets/Scripts/Room Finder Menu/UIRoomFinderList.cs
+++ Assets/Scripts/Room Finder Menu/UIRoomFinderList.cs
@@ -13,14 +13,28 @@ namespace PhotonPunExample
 
         private PrefabFactory<UIRoomFinderListItem> _listItemFactory;
 
-        private void Awake()
+        #region Initialization
+
+        private bool _initialized = false;
+
+        private void Initialize()
         {
+            if (_initialized) return;
+            _initialized = true;
+            
             _listItemFactory = new PrefabFactory<UIRoomFinderListItem>(_pfUIRoomFinderListItem, transform);
 
             UIRoomFinderListItem[] existingItems = GetComponentsInChildren<UIRoomFinderListItem>();
 
             _listItemFactory.GetPool().AddRange(existingItems);
             _listItemFactory.PoolAll();
+        }
+
+        #endregion
+
+        private void Awake()
+        {
+            Initialize();
         }
 
         public void OnDisable()
@@ -30,6 +44,7 @@ namespace PhotonPunExample
 
         public void Populate(List<RoomInfo> roomList)
         {
+            Initialize();
             _listItemFactory.PoolAll();
 
             for (int i = 0; i < roomList.Count; i++)
