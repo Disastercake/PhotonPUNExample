f09a9cb3e54ef8dedb91aca4ddc08258e80c1386
Disastercake <mike@disastercake.com>
2022-09-28 08:54:22 -0400

    Add : PlayerActor.cs - Component that handles the player actor.  Currently just listens to its instantiation event and names itself descriptively to match the owning player.

---
+++ Assets/Scripts/Game Logic/PlayerActor.cs
@@ -0,0 +1,38 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+namespace PhotonPunExample
+{
+    /// <summary>
+    /// Note: IPunInstantiateMagicCallback is called on the exact object PhotonNetwork.Instantiate() created.
+    /// </summary>
+    [DisallowMultipleComponent]
+    public class PlayerActor : MonoBehaviour, IPunInstantiateMagicCallback
+    {
+        
+        private GameObject _gameObject;
+
+        #region Initialization
+
+        private bool _initialized = false;
+
+        private void Initialize()
+        {
+            if (_initialized) return;
+            _initialized = true;
+            _gameObject = gameObject;
+        }
+
+        #endregion
+        
+        void IPunInstantiateMagicCallback.OnPhotonInstantiate(PhotonMessageInfo info)
+        {
+            Initialize();
+            
+            // e.g. Store this GameObject as this player's character in Player.TagObject
+            info.Sender.TagObject = _gameObject;
+            _gameObject.name = $"{_gameObject.name} (Player: {info.Sender.ActorNumber})";
+        }
+        
+    }
+}
