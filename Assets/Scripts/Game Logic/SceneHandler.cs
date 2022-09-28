421e16c0d02c9c4d4704bad9d1393f7628874407
Disastercake <mike@disastercake.com>
2022-09-28 08:38:54 -0400

    Add : SceneHandler.cs - Will listen to PUN callbacks for object instantiation and name them descriptive to their invoking Player.

---
+++ Assets/Scripts/Game Logic/SceneHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Photon.Pun;
+using UnityEngine;
+
+namespace PhotonPunExample
+{
+    [DisallowMultipleComponent]
+    public class SceneHandler : MonoBehaviour, IPunInstantiateMagicCallback
+    {
+        private void OnEnable()
+        {
+            PhotonNetwork.AddCallbackTarget(this);
+        }
+
+        private void OnDisable()
+        {
+            PhotonNetwork.AddCallbackTarget(this);
+        }
+
+        void IPunInstantiateMagicCallback.OnPhotonInstantiate(PhotonMessageInfo info)
+        {
+            Debug.Log("OnPhotonInstantiate()");
+            
+            object[] instantiationData = info.photonView.InstantiationData;
+
+            if (instantiationData.Length <= 0)
+                return;
+
+            info.photonView.gameObject.name = instantiationData[0] as string ?? $"NO_NAME ({info.photonView.ViewID})";
+        }
+    }
+}
