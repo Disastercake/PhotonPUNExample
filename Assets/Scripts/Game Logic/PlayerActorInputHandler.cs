8a8722fc8a56e2acd061b606d62347652ca1008d
Disastercake <mike@disastercake.com>
2022-09-30 19:33:54 -0400

    Add: PlayerActorInputHandler.cs - Handles receiving and routing player input.

---
+++ Assets/Scripts/Game Logic/PlayerActorInputHandler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace PhotonPunExample
+{
+    /// <summary>
+    /// Handles receiving player input and routing it to the correct components.
+    /// </summary>
+    [DisallowMultipleComponent]
+    public class PlayerActorInputHandler : MonoBehaviour
+    {
+        private ActorMotor _actorMotor;
+        private Vector3 _moveVector;
+
+        #region Initialization
+
+        private bool _initialized = false;
+
+        private void Initialize()
+        {
+            if (_initialized) return;
+            _initialized = true;
+
+            _actorMotor = GetComponent<ActorMotor>();
+        }
+
+        #endregion
+
+        private void Awake()
+        {
+            Initialize();
+        }
+
+        private void Update()
+        {
+            _actorMotor.Move(_moveVector);
+        }
+
+        public void OnMoveInput(InputAction.CallbackContext value)
+        {
+            if (!enabled)
+            {
+                Debug.Log("OnMoveInput: " + enabled);
+                return;
+            }
+            
+            Initialize();
+            
+            var moveVector2d = value.ReadValue<Vector2>();
+            
+            if (moveVector2d.magnitude > 1f)
+                moveVector2d.Normalize();
+
+            _moveVector = new Vector3(moveVector2d.x, 0, moveVector2d.y);
+        }
+    }
+}
